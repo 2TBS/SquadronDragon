623c3e20e928e42d276a86e4e851eba7a9bd464a
Akash Basu <33015666+FugaX@users.noreply.github.com>
2017-12-20 12:45:35 -0800

    Main Menu Script
    Script for the Main Menu that allows the player to toggle from the Main Menu to the game

---
+++ Assets/Scripts/MM_Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MM_Menu : MonoBehaviour {
+    int level = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene() {
+		SceneManager.LoadScene(level);
+	}
+    public void BackScene()
+    {
+        SceneManager.LoadScene(level-1);
+        level--;
+    }
+    public void GameOver()
+    {
+        level = 0;
+        SceneManager.LoadScene(level);
+    }
+}
