bae377645862da04ddd2f90693a7cff90c82b195
Ben Cuan <ben.cuan@gmail.com>
2017-12-21 11:36:49 -0800

    Add prefabs and sprites for HUD

---
+++ Assets/Scripts/Enemy/EN_Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EN_Movement : MonoBehaviour {
+
+	public const float MOVE_SPEED = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
