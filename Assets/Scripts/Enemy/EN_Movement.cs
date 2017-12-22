2ce96b10775899cdde50908f092e417dca8e79c6
Omar Hossain <omarepic1@gmail.com>
2017-12-21 17:17:01 -0800

    Update

---
+++ Assets/Scripts/Enemy/EN_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EN_Movement : MonoBehaviour {
+
+	public const float MOVE_SPEED = 0.1f;
+	bool movingLeft = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(movingLeft)
+			transform.Translate(Vector2.left * MOVE_SPEED);
+		else
+			transform.Translate(Vector2.right * MOVE_SPEED);
+
+		if(transform.position.x < -Vars.MAX_WIDTH || transform.position.x > Vars.MAX_WIDTH)
+			movingLeft = !movingLeft;
+	}
+}
